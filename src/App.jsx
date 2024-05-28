cd1c2d83bf96624fbdae8e65912fb6a4e010257f
Ashish kumar <102891997+erashish21@users.noreply.github.com>
2024-05-28 14:18:22 +0530

    login and signup ui complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Login from './components/account/Login'
+
+
+function App() {
+
+
+  return (
+    <div style={{marginTop:64}}>    
+      <Login/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/account/Login.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import { Box, Button, TextField, styled, Typography } from "@mui/material";
+
+//import './Login.css';
+
+const Component = styled(Box)`
+  width: 400px;
+  margin: auto;
+  box-shadow: 5px 2px 5px 2px rgb(0 0 0/0.6);
+`;
+
+const Image = styled("img")({
+  width: 200,
+  margin: "auto",
+  display: "flex",
+  padding: "50px 0 0",
+});
+
+const Wrapper = styled(Box)`
+  padding: 25px 35px;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  & > div,
+  & > button,
+  & > p {
+    margin-top: 20px;
+  }
+`;
+
+const LoginButton = styled(Button)`
+  text-transform: none;
+  background: #ed556a;
+  color: #fff;
+  height: 40px;
+  border-radius: 2px;
+`;
+const SignupButton = styled(Button)`
+  text-transform: none;
+  background: #fff;
+  color: #2874f0;
+  height: 40px;
+  border-radius: 2px;
+  box-shadow: 0 2px 4px 0 rgb(0 0 0/ 20%);
+`;
+
+const Text = styled(Typography)`
+  color: #878787;
+  font-size: 14px;
+`;
+
+const Login = () => {
+  const imageURL =
+    "https://cdn.pixabay.com/photo/2012/05/07/18/57/blog-49006_640.png";
+
+  const [account, toggleAccount] = useState("login");
+
+  const toggleSignup = () => {
+    account === "signup" ? toggleAccount("login") : toggleAccount("signup");
+  };
+
+  return (
+    <Component>
+      <Box>
+        <Image src={imageURL} alt="login" />
+        {account === "login" ? (
+          <Wrapper>
+            <TextField variant="standard" label="Enter username" />
+            <TextField variant="standard" label="Enter password" />
+            <LoginButton variant="contained">Login</LoginButton>
+            <Text style={{ textAlign: "center" }}>OR</Text>
+            <SignupButton onClick={() => toggleSignup()}>
+              Create an account
+            </SignupButton>
+          </Wrapper>
+        ) : (
+          <Wrapper>
+            <TextField variant="standard" label="Enter Name" />
+            <TextField variant="standard" label="Enter username" />
+            <TextField variant="standard" label="Enter password" />
+            <SignupButton>Signup</SignupButton>
+            <Text style={{ textAlign: "center" }}>OR</Text>
+            <LoginButton variant="contained" onClick={() => toggleSignup()}>
+              Already have an account
+            </LoginButton>
+          </Wrapper>
+        )}
+      </Box>
+    </Component>
+  );
+};
+
+export default Login;
